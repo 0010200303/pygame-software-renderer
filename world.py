35860e4a8f6ca49a73251c8423ec608b66c61432
Jonas S <jonasfxcf@outlook.de>
2020-10-12 19:27:22 +0200

    fix merge conflict in get_model_view

--- world.py
+++ world.py
@@ -11,23 +11,16 @@ class Transform:
         self.scale    = scale
     
     def get_model_view(self) -> mat4:
-<<<<<<< HEAD
-        return mat4.translation(self.position) @ mat4.rotation_x(self.rotation.x) @ mat4.rotation_y(self.rotation.y) @ mat4.rotation_z(self.rotation.z) @ mat4.scale(self.scale)
+        matrix = mat4.translation(self.position)
 
-=======
-        m = mat4.translation(self.position)
-
-        if (self.rotation.x != 0):
-            m *= mat4.rotation_x(self.rotation.x)
-        if (self.rotation.y != 0):
-            m *= mat4.rotation_y(self.rotation.y)
-        if (self.rotation.z != 0):
-            m *= mat4.rotation_z(self.rotation.z)
-        if self.scale.x != 1 or self.scale.y != 1 or self.scale.z != 1:
-            m *= mat4.scale(self.scale)
-        return m
+        if (self.rotation.x != 0.0): matrix @= mat4.rotation_x(self.rotation.x)
+        if (self.rotation.y != 0.0): matrix @= mat4.rotation_y(self.rotation.y)
+        if (self.rotation.z != 0.0): matrix @= mat4.rotation_z(self.rotation.z)
+        if self.scale.x != 1.0 or self.scale.y != 1.0 or self.scale.z != 1.0:
+            matrix @= mat4.scale(self.scale)
+        
+        return matrix
     
->>>>>>> 98401e590376dc4b630d981d314905a18f5003b4
     @staticmethod
     def identity() -> 'Transform':
         return Transform(vec3.zero(), vec3.zero(), vec3.one())
